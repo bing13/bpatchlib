ece74b516655913870a7592070733e0ef5d7b2f2
bing13 <bernard9395@gmail.com>
2020-08-15 18:05:02 -0700

    first working version

---
+++ bpatchlib.py
@@ -0,0 +1,178 @@
+###https://pysimplegui.readthedocs.io/
+import PySimpleGUI as sg
+import csv
+from mido import MidiFile, Message, MidiTrack
+import mido
+import sys, pause, threading
+
+def read_patch_file(inf):
+    voice = {}
+    pnames = []
+    PATCHDIR = 'Patch_Files/'
+    with open(PATCHDIR+inf, 'r') as INF:
+        ireader = csv.reader(INF, delimiter='\t')
+        #(num, section, cat, name, msb, lsb, patch)
+        for row  in ireader:
+            if row[0] != 'num':
+                # let's keep an list of patch names in the order they were read in
+                #print(row[0],row[3])
+                pnames.append(row[3].strip())
+                
+                if inf == 'RD-800_Final_Sound_List.csv':
+                    row[6] = int(row[6]) -1
+                else:
+                    row[6] = row[6].strip()
+                voice[row[3].strip()]={ 'name':row[3], 'num':row[0], 'section':row[1], \
+                                        'category':row[2],'msb':row[4],'lsb':row[5],'patch':row[6] }
+    return voice,pnames
+
+#voice['patchname']['num', 'section', 'category', 'msb', 'lsb', 'patch']
+def mido_stuff():
+    ## inf = '/home/bernard/Bitwig Studio/Projects/00 coversion experiments/Texas Ideal v2 2019-12.mid'
+    ## inf = 'media/bernard/Extreme SSD/20_MIDI_clips/MIDI Library/Groove Monkee/Country/065 Ballad Ride F3 P.mid'
+    ports = mido.get_input_names()
+    #NOTE: THE PORT NUMBERS ARE DYNAMIC. But the order should be reliable (I hope)
+    # ['EDIROL UM-550:EDIROL UM-550 MIDI 1 20:0', 'EDIROL UM-550:EDIROL UM-550 MIDI 2 20:1', 
+    #        'EDIROL UM-550:EDIROL UM-550 MIDI 3 20:2', 'EDIROL UM-550:EDIROL UM-550 MIDI 4 20:3',
+    #        'EDIROL UM-550:EDIROL UM-550 MIDI 5 20:4', 'EDIROL UM-550:EDIROL UM-550 MIDI 6 20:5',
+    #        'Midi Through:Midi Through Port-0 14:0', 'Scarlett 6i6 USB:Scarlett 6i6 USB MIDI 1 28:0']
+    return ports
+
+def describe_tracks(midi_file):
+    print("\nFile:",midi_file.filename)
+
+def some_notes(chanx):
+    notes = [
+        Message('note_on', note=67, velocity=45, time=0, channel=chanx),
+        Message('note_off', note=67, channel=chanx),
+        Message('note_on', note=42, velocity=45, time=0, channel=chanx),
+        Message('note_off', note=42, channel=chanx),
+        Message('note_on', note=87, velocity=45, time=0, channel=chanx),
+        Message('note_off', note=87, channel=chanx),
+        Message('note_on', note=27, velocity=45, time=0, channel=chanx),
+        Message('note_off', note=27,  channel=chanx)
+        ]
+    return notes
+    
+
+def play_some_notes(portx, channel, dv ):
+    pm = build_patch_message(channel, dv)
+    for p in pm:
+        portx.send(p)
+    nm = some_notes(channel)
+    for n in nm:
+        portx.send(n)
+        pause.seconds(0.15)
+
+def build_patch_message(channelx, dv):
+    # Bank Select selects the desired Bank for the specified Channel.
+    # The first byte listed is the MSB, transmitted on cc#0.
+    # The second byte listed is the LSB, transmitted on cc#32.
+    out_mess = []
+    if dv['msb'] and dv['lsb'] != '':
+        # send msb
+        out_mess.append(Message('control_change', control =  0, value = int(dv['msb']), channel = channelx))
+        # send lsb
+        out_mess.append(Message('control_change', control = 32, value = int(dv['lsb']), channel = channelx))
+    # send patch
+    out_mess.append(Message('program_change', program = int(dv['patch']),             channel = channelx))
+    return out_mess
+
+###################################################################################
+## port setup
+ports = mido_stuff()
+rd_port = mido.open_output(ports[1])
+prot_port = mido.open_output(ports[2])
+mt_port = mido.open_output(ports[3])
+## read in patch files for each sound module
+mt32_patches = read_patch_file('MT-32_Final_sound_list.csv')
+prot_patches = read_patch_file('proteus1_Final_sound_list.csv')
+rd800_patches = read_patch_file('RD-800_Final_Sound_List.csv')
+dvoice= rd800_patches
+# dvoice[0] is the dict, keyed on voice name, for looking up parameters
+# dvoice[1] is an array of the simple list of patch names, in the order they appear in the input file
+#sg.theme('DarkAmber')   # Add a touch of color
+
+#####
+layout = [  
+    [sg.Text('Module',size=(8,1),text_color='black'),
+     sg.Radio('RD-800', "Module_Selector", default=True, key="rdkey",enable_events=True),
+     sg.Radio('Proteus 1', "Module_Selector", key="protkey",enable_events=True),
+     sg.Radio('MT-32', "Module_Selector", key="mtkey",enable_events=True)],
+    
+    [sg.Text('Patch',size=(8,1),text_color='black'),
+     sg.Text('Num',size=(4,1),key='num'),
+     sg.Text('Name',size=(20,1),key='name')],
+    [sg.Text('Section',size=(8,1),text_color='black'), sg.Text(key='section',size=(20,1))],
+    
+    [sg.Text('Category',size=(8,1),text_color='black'), sg.Text(key='category',size=(20,1))] ,
+    
+    [sg.Text('MSB',size=(8,1),text_color='black'), sg.Text(key='msb',size=(5,1)),
+     sg.Text('LSB',size=(5,1),text_color='black'), sg.Text(key='lsb',size=(5,1)),
+     sg.Text('Patch',size=(5,1),text_color='black'), sg.Text(key='patch',size=(5,1)) ],
+    
+    [sg.Text('Search',size=(8,1),text_color='black'),
+     sg.InputText(key='searchx',size=(15,1),enable_events=True),
+     sg.Button('vsearch'), sg.Button('vclear')],
+    
+    [sg.Listbox(values=dvoice[1], size=(40, 30),enable_events=True,key='voice' )],
+    [sg.Button('Ok'), sg.Button('Cancel') ] ]
+
+# Create the Window
+window = sg.Window('Patch Librarian v.0.2', layout)
+## initialize
+dvoice = rd800_patches
+this_v = 'Concert Grand (*1)'
+this_port = rd_port            
+
+while True:
+    event, values = window.read()
+    #,print("event=",event,"values=",values)
+    if event == sg.WIN_CLOSED or event == 'Cancel': # if user closes window or clicks cancel
+        break
+    else:
+        if event == 'voice':
+            this_v = values['voice'][0]
+        elif event in ['vsearch','searchx']:
+            searchx = values['searchx']
+            filtered_patches = [ x for x in dvoice[1] if searchx.lower() in x.lower() ]
+            window['voice'].update(filtered_patches)
+        elif event == 'vclear':
+            window['voice'].update(dvoice[1])
+        elif event == 'rdkey':
+            dvoice = rd800_patches
+            this_v = 'Concert Grand (*1)'
+            this_port = rd_port            
+            window['voice'].update(dvoice[1])
+        elif event == 'protkey':
+            dvoice = prot_patches
+            this_v = 'Stereo Piano'
+            this_port = prot_port
+            window['voice'].update(dvoice[1])
+        elif event == 'mtkey':
+            dvoice = mt32_patches
+            this_v = 'Acou Piano 1'
+            this_port = mt_port
+            window['voice'].update(dvoice[1])
+        else:
+            print("ERROR!  ====>  EVENT UNKNOWN <===============")
+        dv = dvoice[0][this_v]
+        window['num'].update(dv['num'])
+        window['name'].update(dv['name'])        
+        window['section'].update(dv['section'])
+        window['category'].update(dv['category'])        
+        window['msb'].update(dv['msb'])
+        window['lsb'].update(dv['lsb'])
+        window['patch'].update(dv['patch'])
+        
+        if event == 'voice':
+           play_some_notes(this_port,0,dv)
+
+window.close()
+
+rd_port.reset()
+rd_port.close()
+prot_port.reset()
+prot_port.close()
+mt_port.reset()
+mt_port.close()
